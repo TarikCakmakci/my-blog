56d44a95477c17520dd400ab9fe41565e18eaf74
TarikCakmakci <tarikcakmakci@outlook.com>
2021-08-13 19:01:01 +0300

    Update topbar

---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.css"
+
+export default function topbar() {
+    return (
+        <div className="top">
+            
+           <p>Anasayfa</p>
+         
+        </div>
+    )
+}
