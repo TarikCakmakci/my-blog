cb9d3cda82ed96f4071c730a04d3020aa3b3699e
TarikCakmakci <tarikcakmakci@outlook.com>
2021-08-16 17:40:38 +0300

    Topbar update
    Home, about, contact added

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -3,8 +3,23 @@ import "./topbar.css"
 export default function topbar() {
     return (
         <div className="top">
-            
-           <p>Anasayfa</p>
+            <div className="topLeft">
+                <i className="topIcon fab fa-linkedin"></i>
+                <i className="topIcon fab fa-github"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">Anasayfa</li>
+                    <li className="topListItem">Hakkımda</li>
+                    <li className="topListItem">İletişim</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topImg" src="https://img.icons8.com/ios-glyphs/30/000000/user-male--v2.png"alt=""/>
+                <ul className="signItem">Giriş</ul>
+                <i className="topSearchIcon fas fa-search"></i>
+            </div>
+           
          
         </div>
     )
