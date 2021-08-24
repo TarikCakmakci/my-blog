88c084b1e8542cb670126d8df80aee20bfbebf65
TarikCakmakci <tarikcakmakci@outlook.com>
2021-08-24 13:57:28 +0300

    Update
    Sidebar update

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -5,22 +5,22 @@ export default function sidebar() {
         <div className="sidebar">
             <div className="sidebarItem">
                 <span className="sidebarTitle">HAKKIMDA</span>
-                <img className="aboutMeImg" src="http://www.themarketingsage.com/wp-content/uploads/2015/08/about-me-leon-severan-we-buy-houses.jpg" alt=""/>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto at labore, vel, illum vitae ipsum odio delectus iste unde officia quas.</p>
             </div>
         <div className="sidebarItem">
         <span className="sidebarTitle">KATEGORİLER</span>
         <ul className="sidebarList">
-            <li className="sidebarListItem">Life</li>
-            <li className="sidebarListItem">Music</li>
-            <li className="sidebarListItem">Style</li>
-            <li className="sidebarListItem">Sport</li>
-            <li className="sidebarListItem">Tech</li>
+            <li className="sidebarListItem">Hayat</li>
+            <li className="sidebarListItem">Müzik</li>
+            <li className="sidebarListItem">Giyim</li>
+            <li className="sidebarListItem">Spor</li>
+            <li className="sidebarListItem">Teknoloji</li>
             <li className="sidebarListItem">Sinema</li>
+            <li className="sidebarListItem">Ahmet</li>
          </ul>
         </div>
         <div className="sidebarItem">
-             <span className="sidebarTitle">FOLLOW US</span>
+             <span className="sidebarTitle">FOLLOW ME</span>
              <div className="sidebarSocial">
                 <i className="sidebarIcon fab fa-linkedin"></i>
                 <i className="sidebarIcon fab fa-github"></i>
