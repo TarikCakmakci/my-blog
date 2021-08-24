155cc5377dff12736ee6111f50ec720b146e3066
TarikCakmakci <tarikcakmakci@outlook.com>
2021-08-24 11:58:53 +0300

    Update

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,7 +3,11 @@ import "./header.css"
 export default function header() {
     return (
         <div className="header">
-            header x
+           <div className="headerTitles">
+                <span className="headerTitleSm">X</span>
+                <span className="headerTitleLg">Blog</span>
+            </div>
+            <img className="headerImg" src="http://lava360.com/wp-content/uploads/2014/01/Classic-Background-Images-For-Wordpress-Blogs-26.jpg" alt=""></img>
         </div>
     )
 }
