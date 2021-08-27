022935f52e47c67231d7a94d3879956db6a9837a
TarikCakmakci <tarikcakmakci@outlook.com>
2021-08-27 18:32:27 +0300

    Update
    Write page updated

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -2,8 +2,27 @@ import "./write.css"
 
 export default function Write() {
     return (
-        <div>
-            write
+        <div className="write">
+            <img 
+                className="writeImg" src="https://picsum.photos/4000/4024" alt="" />
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                        <i className="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{ display: "none" }} />
+                    <input type="text" placeholder="Başlık girmelisin.." className="writeInput" autoFocus={true} />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder="Anlat sen seversin..."
+                        type="text" className="writeInput writeText">
+                    </textarea>
+                </div>
+                <button className="writeSubmit">
+                    Yükle
+                </button>
+
+            </form>
         </div>
     )
 }
