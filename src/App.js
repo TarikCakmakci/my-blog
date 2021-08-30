11bc034fede72b74a1febcb4820896be9499ff82
TarikCakmakci <tarikcakmakci@outlook.com>
2021-08-31 01:07:21 +0300

    Update
    React-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import Topbar from "./components/topbar/Topbar";
 import Register from "./pages/register/Register";
-
+import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
+import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <>
-      <Topbar />
-      <Register />
-      </>
+    
+    <Router>
+        <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        
+        
+        
+      </Switch>
+    </Router>
+     
   );
 }
 export default App;
