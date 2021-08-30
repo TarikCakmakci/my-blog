9980a20f123c15db64ae56f5fb64f57e6bdafed8
TarikCakmakci <tarikcakmakci@outlook.com>
2021-08-31 00:18:35 +0300

    Update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,24 @@
 import "./login.css"
-import loginBackground from '../../components/images/loginBackground.png'
+
 
 export default function Login() {
     return (
         <div className="login">
-            <img className="loginBackgroundImg" src={loginBackground}alt=""/>
+            <span className="loginTitle">Giriş</span>
+        <form className="loginForm">
+            <label>Kullanıcı Adı</label>
+                <input type="text" className="loginInput" placeholder="Kullanıcı adınızı giriniz..."></input>
+            <label>Email</label>
+                <input type="text" className="loginInput" placeholder="Mail adresinizi giriniz..."></input>
+            <label>Şifre</label>
+                <input type="password" className="loginInput" placeholder="Şifrenizi giriniz..."></input>    
+            <button className="loginButton">Giriş Yap</button>
+            <button className="loginRegisterButton">Kayıt Ol</button>      
+        </form>
         
-        </div>
+        
+
+    </div>
        
     )
 }
