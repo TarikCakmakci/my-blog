1ccd3e9bd777e39fae5b3312f7753c466dce8f84
TarikCakmakci <tarikcakmakci@outlook.com>
2021-09-21 14:55:02 +0300

    Update
    Connected to mongoDB. Added express

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,25 @@
+import "./login.css"
+import { Link } from "react-router-dom"
+
+export default function Login() {
+    return (
+        <div className="login">
+            <span className="loginTitle">GİRİŞ</span>
+        <form className="loginForm">
+            <label>Kullanıcı Adı</label>
+                <input type="text" className="loginInput" placeholder="Kullanıcı adınızı giriniz..."></input>
+            <label>Email</label>
+                <input type="text" className="loginInput" placeholder="Mail adresinizi giriniz..."></input>
+            <label>Şifre</label>
+                <input type="password" className="loginInput" placeholder="Şifrenizi giriniz..."></input>    
+            <button className="loginButton">Giriş Yap</button>
+            <button className="loginRegisterButton">
+            <Link to="/register" className="link"></Link>Kayıt Ol</button>      
+        </form>
+        
+        
+
+    </div>
+       
+    )
+}
