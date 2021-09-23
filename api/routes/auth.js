d5b50f4b6ce217a6d41129f3ba394aae3fe54e65
TarikCakmakci <tarikcakmakci@outlook.com>
2021-09-23 21:05:42 +0300

    Update
    REST API

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,16 +5,16 @@ const bcrypt = require('bcrypt');
 
 
 //REGISTER
-router.post("/register",async(req,res) => {
+router.post("/register", async (req, res) => {
 
     try {
 
         const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(req.body.password,salt);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:hashedPass,
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass,
         });
         const user = await newUser.save();
         res.status(200).json(user);
@@ -27,9 +27,9 @@ router.post("/register",async(req,res) => {
 });
 //LOGIN
 
-router.post("/login",async(req,res)=> {
+router.post("/login", async (req, res) => {
     try {
-        const user = await User.findOne({username:req.body.username})
+        const user = await User.findOne({ username: req.body.username })
         !user && res.status(400).json("Kullanıcı adı veya şifre yanlış!")
 
         const validated = await bcrypt.compare(req.body.password, user.password)
